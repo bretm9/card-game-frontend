83ee9b0eb53253541de76ba7a0f1da1dad11d97d
Bret Merritt <merrittbret9@gmail.com>
2021-03-18 16:43:27 -0600

    Add routes to Home and Game, redirect Switch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import React from 'react';
+import io from 'socket.io-client';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/game' component={Game} />
+        <Redirect to='/' />
+      </Switch>
+    </main>
   );
 }
 
