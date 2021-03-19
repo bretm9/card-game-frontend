b602c3e6a01931dec76a77cd487173b6508156f8
Bret Merritt <merrittbret9@gmail.com>
2021-03-19 11:21:28 -0600

    Game renders map of OpponentHands, a Table, and a PlayerHand

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import OpponentHand from '../OpponentHand/OpponentHand'
+import Table from '../Table/Table'
+import PlayerHand from '../PlayerHand/PlayerHand'
 
 const Game = (props) => {
+  const opponentHands = props.opponents.map((opponent, i) => {
+    return <OpponentHand key={i} player={opponent} />
+  });
+  
   return ( 
     <div>
       <h1>You are player {props.playerNumber}</h1>
+      {opponentHands}
+      <Table />
+      <PlayerHand />
     </div>
   )
 }
