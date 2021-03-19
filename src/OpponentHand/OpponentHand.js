15409ec8a9cdd4a4c3bb6a5f9701b2f7900aa638
Bret Merritt <merrittbret9@gmail.com>
2021-03-18 18:19:37 -0600

    Add empty component

---
+++ src/OpponentHand/OpponentHand.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OpponentHand() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OpponentHand
---
+++ src/PlayerHand/PlayerHand.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlayerHand() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlayerHand
