b4943238158aeb2cc4354ebde457eb1b334c093f
Bret Merritt <merrittbret9@gmail.com>
2021-03-19 10:54:50 -0600

    Refactor to class based component, add join Socket.IO method, pass join to Game

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import io from 'socket.io-client';
+import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import logo from '../logo.svg';
 import './App.css';
 
-import Home from '../Home/Home'
-import Game from '../Game/Game'
+import Home from '../Home/Home';
+import Game from '../Game/Game';
 
-function App() {
-  return (
-    <main>
-      <Switch>
-        <Route exact path='/' render={Game} />
-        {/* <Route path='/game' render={Game} /> */}
-        <Redirect to='/' />
-      </Switch>
-    </main>
-  );
+import io from 'socket.io-client';
+
+const ENDPOINT = 'http://localhost:9000';
+
+const socket = io.connect(ENDPOINT);
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      playerNumber: null
+    };
+  }
+
+  join = () => {
+    socket.emit('join', (playerNumber) => {
+      this.setState({ playerNumber: playerNumber });
+    })
+  }
+
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route 
+            exact 
+            path='/' 
+            render={() => {
+              return <Game join={this.join} playerNumber={this.state.playerNumber}/>
+            }} 
+          />
+          {/* <Route path='/game' render={Game} /> */}
+          <Redirect to='/' />
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;
